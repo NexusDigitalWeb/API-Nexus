2633a2c6a25280eeadf64122dc6502b8b3b5a80d
Tomas <tomascerdeyraimport@gmail.com>
2024-01-16 03:21:41 -0300

    desktop questions

--- client/src/components/questions/Questions.tsx
+++ client/src/components/questions/Questions.tsx
@@ -66,61 +66,63 @@ const Questions = (): React.ReactElement => {
     <div className="flex flex-col gap-5 mt-14" id="frequentQuestions">
       <div className="h-[175px] items-center justify-center flex flex-col gap-[30px] relative left-[1000px] title-animation">
         <h2 className="flex flex-col items-center justify-center w-[226px] h-[97px]">
-          <span className="w-[226px] md:w-[326px]  h-[46px] md:h-[66px] rounded-[7px] pr-[7px] pl-[7px] text-center text-[36px] md:text-[46px] lead-[45.94px] bg-[#B9FF66] gap-[]">
+          <span className="w-[226px] md:w-[326px] lg:w-[426px] 2xl:w-[526px] h-[46px] md:h-[66px] lg:h-[86px] 2xl:h-[106px] rounded-[7px] pr-[7px] pl-[7px] text-center text-[36px] md:text-[46px] lg:text-[56px] 2xl:text-[66px] lead-[45.94px] bg-[#B9FF66] gap-[]">
             Preguntas
           </span>
-          <span className="w-[240px] md:w-[280px] h-[51px] md:h-[61px] rounded-[7px] pr-[7px] pl-[7px] text-center text-[40px] lead-[51.04px] gap-[10px] bg-[#B9FF66]">
+          <span className="w-[240px] md:w-[280px] lg:w-[320px] 2xl:w-[420px] h-[51px] md:h-[61px] lg:h-[71px] 2xl:h-[81px] rounded-[7px] pr-[7px] pl-[7px] text-center text-[40px] lg:text-[45px] 2xl:text-[50px] lead-[51.04px] gap-[10px] bg-[#B9FF66] mb-10">
             Frecuentes
           </span>
         </h2>
-        <p className="text-[16px] md:text-[18px] lead-[24px] text-center p-1">
+        <p className="text-[16px] md:text-[18px] lg:text-[26px] lead-[24px] text-center p-1">
           Verifica las preguntas frecuentes para sacar tus dudas
         </p>
         <Waypoint onEnter={() => handleWaypointToRight(".title-animation")} />
       </div>
 
-      <div className="p-2 flex flex-col gap-3">
+      <div className="p-2 flex flex-col items-center gap-3">
         {questionsList.map((quest, index) => (
-          <div
-            key={quest._id}
-            className={`relative left-[1000px] animation-window-${index}`}
-          >
-            <Waypoint
-              onEnter={() =>
-                handleWaypointToRight(`.animation-window-${index}`)
-              }
-            />
+          <>
             <div
-              className={`h-[100px] rounded-[45px] border border-b-4 border-black border-solid flex flex-row items-center justify-around bg-[#F3F3F3]`}
+              key={quest._id}
+              className={`relative w-full flex flex-col items-center left-[2000px] animation-window-${index}`}
             >
-              <p className="text-[30px] md:text-[40px] font-medium">0{index + 1}</p>
-              <p className="text-center text-[12px] sm:text-[15px] md:text-[18px]  w-[150px] sm:w-[400px] md:w-[450px] min-[500px]:w-[300px]">
-                {quest.question}
-              </p>
-              <p
-                onClick={() => AnswerVisibility(index)}
-                className="border border-black rounded-[100%] w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-[20px] flex items-center justify-center cursor-pointer"
+              <Waypoint
+                onEnter={() =>
+                  handleWaypointToRight(`.animation-window-${index}`)
+                }
+              />
+              <div
+                className={`h-[100px] lg:h-[150px] w-full max-w-[1400px] rounded-[45px] border border-b-4 border-black border-solid flex flex-row items-center justify-around bg-[#F3F3F3]`}
               >
-                {isClicked && openQuestionIndex === index ? "-" : "+"}
-              </p>
-            </div>
+                <p className="text-[30px] md:text-[40px] lg:text-[50px] font-medium">0{index + 1}</p>
+                <p className="text-center text-[12px] sm:text-[15px] md:text-[18px] lg:text-[20px] 2xl:text-[26px] w-[150px] sm:w-[400px] md:w-[450px] min-[500px]:w-[300px] lg:w-[550px] 2xl:w-[700px]">
+                  {quest.question}
+                </p>
+                <p
+                  onClick={() => AnswerVisibility(index)}
+                  className="border border-black rounded-[100%] w-[30px] h-[30px] md:w-[40px] md:h-[40px] lg:w-[50px] lg:h-[50px] 2xl:w-[60px] 2xl:h-[60px] text-[20px] lg:text-[35px] 2xl:text-[40px] flex items-center justify-center cursor-pointer"
+                >
+                  {isClicked && openQuestionIndex === index ? "-" : "+"}
+                </p>
+              </div>
 
+
+            </div>
             <div
-              className={`h-[full] rounded-[50px] text-center border-black border-solid flex flex-row items-center justify-around bg-[#B9FF66] transition-all duration-200 ease-in
-                 ${
-                  visibleAnswers[index]?.isVisible &&
+              className={`h-[full] w-full max-w-[1400px] rounded-[50px] text-center border-black border-solid flex flex-row items-center justify-around bg-[#B9FF66] transition-all duration-200 ease-in
+                 ${visibleAnswers[index]?.isVisible &&
                   openQuestionIndex === index
-                    ? "flex h-full p-3"
-                    : "h-0"
+                  ? "flex h-full p-3"
+                  : "h-0"
                 }`}
             >
-              <p className="text-[12px] sm:text-[16px] md:text-[18px] w-[250px] sm:w-[420px] md:w-[580px] transition-all ease-in duration-150">
+              <p className="text-[12px] sm:text-[16px] md:text-[18px] lg:text-[20px] 2xl:text-[22px] w-[250px] sm:w-[420px] md:w-[580px] 2xl:w-[800px] lg:w-[660px] transition-all ease-in duration-150">
                 {visibleAnswers[index]?.isVisible
                   ? visibleAnswers[index].response
                   : ""}
               </p>
             </div>
-          </div>
+          </>
         ))}
       </div>
     </div>
